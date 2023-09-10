e7b85e7762403f9295b4dbbb09f6bfe7638ba1b5
Sanket <sanketchaudhary1412@gmail.com>
2023-09-10 20:21:07 +0200

    Added design patterns

---
+++ src/structural_design_pattern/bridge_pattern/BridgePattern.java
@@ -0,0 +1,53 @@
+package structural_design_pattern.bridge_pattern;
+
+interface DrawAPI {
+    void drawCircle(int radius, int x, int y);
+}
+class RedCircle implements DrawAPI {
+    @Override
+    public void drawCircle(int radius, int x, int y) {
+        System.out.println("Drawing Circle[ color: red, radius: " + radius + ", x: " + x + ", y: " + y + "]");
+    }
+}
+
+class GreenCircle implements DrawAPI {
+    @Override
+    public void drawCircle(int radius, int x, int y) {
+        System.out.println("Drawing Circle[ color: green, radius: " + radius + ", x: " + x + ", y: " + y + "]");
+    }
+}
+
+abstract class Shape {
+    protected DrawAPI drawAPI;
+
+    protected Shape(DrawAPI drawAPI){
+        this.drawAPI = drawAPI;
+    }
+    public abstract void draw();
+}
+
+class Circle extends Shape {
+    private int x, y, radius;
+
+    public Circle(int x, int y, int radius, DrawAPI drawAPI) {
+        super(drawAPI);
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public void draw() {
+        drawAPI.drawCircle(radius,x,y);
+    }
+}
+
+public class BridgePattern {
+    public static void main(String[] args) {
+        Shape redCircle = new Circle(50,50, 10, new RedCircle());
+        Shape greenCircle = new Circle(50,50, 10, new GreenCircle());
+
+        redCircle.draw();
+        greenCircle.draw();
+    }
+
+}
